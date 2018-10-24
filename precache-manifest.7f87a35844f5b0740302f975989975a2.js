9606dd7409c3275bbb07e72709d967efc9bd454a
MatthewDodi <matthew.dodi@gmail.com>
2018-10-23 21:50:36 -0700

    Updates

---
+++ precache-manifest.7f87a35844f5b0740302f975989975a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a1957e1225b0361caceb1cbdba599ef",
+    "url": "/perused-book-web-app/static/media/black.1a1957e1.jpg"
+  },
+  {
+    "revision": "1bc7f3dbc0cd349e15a0",
+    "url": "/perused-book-web-app/static/js/runtime~main.1bc7f3db.js"
+  },
+  {
+    "revision": "9cc46baea6564527e292",
+    "url": "/perused-book-web-app/static/js/main.9cc46bae.chunk.js"
+  },
+  {
+    "revision": "67eaa502c31c9178afa7",
+    "url": "/perused-book-web-app/static/js/1.67eaa502.chunk.js"
+  },
+  {
+    "revision": "9cc46baea6564527e292",
+    "url": "/perused-book-web-app/static/css/main.35e01466.chunk.css"
+  },
+  {
+    "revision": "116b621ac742c7800c2576a90b194d5f",
+    "url": "/perused-book-web-app/index.html"
+  }
+];
